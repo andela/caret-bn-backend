68bca7fa1a8b765d717ab9fa136a0a06106de616
sabin6341@gmail.com <izererogersabin@Izeres-MacBook-Pro.local>
2019-09-30 21:00:17 +0200

    chore(testing):setup test settings - setup test settings [Finishes #168781667]

---
+++ src/tests/test.js
@@ -0,0 +1,16 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../index';
+chai.should();
+chai.use(chaiHttp);
+
+describe('server test', () => {
+    it('it should not run the server ', (done) => {
+      chai.request(app)
+        .get('/api/')
+        .end((err, res) => {
+          res.should.have.property('status').eql(404);
+          done();
+        });
+    });
+});
