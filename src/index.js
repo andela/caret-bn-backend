426bce1ab2ba6e8997ff8fc0b6927ae1e6c50a2f
sabin6341@gmail.com <izererogersabin@Izeres-MacBook-Pro.local>
2019-09-30 21:00:17 +0200

    chore(testing):setup test settings - setup test settings [Finishes #168781667]

--- src/index.js
+++ src/index.js
@@ -1,96 +1,27 @@
 import dotenv from 'dotenv';
-const fs = require("fs"),
-    http = require("http"),
-    path = require("path"),
-    methods = require("methods"),
-    express = require("express"),
-    bodyParser = require("body-parser"),
-    session = require("express-session"),
-    cors = require("cors"),
-    passport = require("passport"),
-    errorhandler = require("errorhandler"),
-    mongoose = require("mongoose");
-
-const isProduction = process.env.NODE_ENV === "production";
-
+import express from 'express';
 // Create global app object
-const app = express();
 dotenv.config();
-app.use(cors());
 
-// Normal express config defaults
-app.use(require('morgan')('dev'));
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(require('method-override')());
-
-app.use(express.static(`${__dirname  }/public`));
-
-app.use(
-  session({
-    secret: 'authorshaven',
-    cookie: { maxAge: 60000 },
-    resave: false,
-    saveUninitialized: false
-  })
-);
-
-if (!isProduction) {
-  app.use(errorhandler());
-}
-
-if (isProduction) {
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect('mongodb://localhost/conduit');
-  mongoose.set('debug', true);
-}
-
-require('./models/User');
-
-app.use(require('./routes'));
-
-// / catch 404 and forward to error handler
-app.use((req, res, next) => {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
-});
-
-// / error handlers
-
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-  app.use((err, req, res, next) => {
-        console.log(err.stack);
-
-        res.status(err.status || 500);
-
-        res.json({
-            errors: {
-                message: err.message,
-                error: err
-            }
-        });
+app.use(express.urlencoded({ extended:false }));
+app.use(express.json());
+    
+app.get('/', (req, res) => {
+    res.status(200).json({
+      status: 200,
+      message: 'Welcome to Barefoot Nomad!',
     });
-}
+  });
 
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-        errors: {
-            message: err.message,
-            error: {}
-        }
-    });
+app.use('*',(req,res)=>{
+  res.status(400).json({
+    status: 400,
+    message:'Sorry this router does not exist !',
+  });
 });
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Barefoot Nomad is runnig server On port ${port}...`));
 
-// finally, let's start our server...
-const server = app.listen(process.env.PORT || 3000, () => {
-    console.log("Listening on port " + server.address().port);
-});
+export default app;
