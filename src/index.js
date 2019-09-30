750756d58b4f739665fd7d028879e50502996ae2
eric ngirababyeyi <ericngirababyeyi@erics-MacBook-Pro-2.local>
2019-09-30 13:58:07 +0200

    chore(configure eslint): - add eslintrc.json - install eslint [finishes #168781664]

--- src/index.js
+++ src/index.js
@@ -19,50 +19,52 @@ dotenv.config();
 app.use(cors());
 
 // Normal express config defaults
-app.use(require("morgan")("dev"));
+app.use(require('morgan')('dev'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(require("method-override")());
-app.use(express.static(__dirname + "/public"));
+app.use(require('method-override')());
+
+app.use(express.static(`${__dirname  }/public`));
 
 app.use(
-    session({
-        secret: "authorshaven",
-        cookie: { maxAge: 60000 },
-        resave: false,
-        saveUninitialized: false
-    })
+  session({
+    secret: 'authorshaven',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
 );
 
 if (!isProduction) {
-    app.use(errorhandler());
+  app.use(errorhandler());
 }
 
 if (isProduction) {
-    mongoose.connect(process.env.MONGODB_URI);
+  mongoose.connect(process.env.MONGODB_URI);
 } else {
-    mongoose.connect("mongodb://localhost/conduit");
-    mongoose.set("debug", true);
+  mongoose.connect('mongodb://localhost/conduit');
+  mongoose.set('debug', true);
 }
 
-require("./models/User");
+require('./models/User');
 
-app.use(require("./routes"));
+app.use(require('./routes'));
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// / catch 404 and forward to error handler
+app.use((req, res, next) => {
     const err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
 
-/// error handlers
+// / error handlers
 
 // development error handler
 // will print stacktrace
 if (!isProduction) {
-    app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
         console.log(err.stack);
 
         res.status(err.status || 500);
@@ -78,7 +80,7 @@ if (!isProduction) {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({
         errors: {
@@ -89,6 +91,6 @@ app.use(function(err, req, res, next) {
 });
 
 // finally, let's start our server...
-const server = app.listen(process.env.PORT || 3000, function() {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log("Listening on port " + server.address().port);
 });
