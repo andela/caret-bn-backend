3b9a6ca369aa5a0cbbd858a1c387da4a25d28066
ngireric123 <ngireric123@gmail.com>
2019-10-02 18:20:02 +0200

    Ch(sequelize): set up /sequelize
            - Resolved eslint rules
            [finishes #168781663]

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -3,6 +3,6 @@ import database from '../database/models';
 export default class UserServices {
   static async allUsers() {
     database.users.findAll()
-      .then((users) => users).catch((err) => err);
+      .then(users => users).catch(err => err);
   }
 }
