f5aa14d87008aa8d762d88e3cc3ef5c6cada6cfa
Macheza Dzabala <dzabalamacheza@gmail.com>
2019-10-02 14:21:54 +0200

    Committed

--- src/index.js
+++ src/index.js
@@ -85,8 +85,7 @@ app.use((err, req, res, next) => {
 });
 
 // finally, let's start our server...
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port ${server.address().port}`);
+app.listen(process.env.PORT || 3000, () => {
 });
 
 export default app;
