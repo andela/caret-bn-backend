bd4c2a84f14e65e514b37565808c48e039bdbe11
Macheza Dzabala <dzabalamacheza@gmail.com>
2019-10-02 13:58:42 +0200

    Init Commit

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,16 +1,17 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../index';
+
 chai.should();
 chai.use(chaiHttp);
 
 describe('server test', () => {
-    it('it should not run the server ', (done) => {
-      chai.request(app)
-        .get('/api/')
-        .end((err, res) => {
-          res.should.have.property('status').eql(404);
-          done();
-        });
-    });
+  it('it should not run the server ', (done) => {
+    chai.request(app)
+      .get('/api/')
+      .end((err, res) => {
+        res.should.have.property('status').eql(404);
+        done();
+      });
+  });
 });
