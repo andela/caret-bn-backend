f976b54838c2059f9695133da3a13bfc2a4d65b8
sabin6341@gmail.com <izererogersabin@Izeres-MacBook-Pro.local>
2019-10-02 14:57:16 +0200

    Pre Rebase Commit

--- src/index.js
+++ src/index.js
@@ -5,23 +5,23 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.urlencoded({ extended:false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-    
 app.get('/', (req, res) => {
-    res.status(200).json({
-      status: 200,
-      message: 'Welcome to Barefoot Nomad!',
-    });
+  res.status(200).json({
+    status: 200,
+    message: 'Welcome to Barefoot Nomad!',
   });
+});
 
-app.use('*',(req,res)=>{
+app.use('*', (req, res) => {
   res.status(400).json({
     status: 400,
-    message:'Sorry this router does not exist !',
+    message: 'Sorry this router does not exist !',
   });
 });
 const port = process.env.PORT || 3000;
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Barefoot Nomad is runnig server On port ${port}...`));
 
 export default app;
--- src/tests/test.js
+++ src/tests/test.js
@@ -1,29 +1,32 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import { describe, it } from 'mocha';
 import app from '../index';
+
+
 chai.should();
 chai.use(chaiHttp);
 
 describe('server test', () => {
-    it('it should run the server ', (done) => {
-      chai.request(app)
-        .get('/')
-        .end((err, res) => {
-          res.should.have.property('status').eql(200);
-          res.body.should.have.property('message').eql('Welcome to Barefoot Nomad!');
-          done();
-        });
-    });
+  it('it should run the server ', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.property('status').eql(200);
+        res.body.should.have.property('message').eql('Welcome to Barefoot Nomad!');
+        done();
+      });
+  });
 });
 
 describe('router test', () => {
-    it('it should test wrong routes ', (done) => {
-      chai.request(app)
-        .get('/pi')
-        .end((err, res) => {
-          res.should.have.property('status').eql(400);
-          res.body.should.have.property('message').eql('Sorry this router does not exist !');
-          done();
-        });
-    });
+  it('it should test wrong routes ', (done) => {
+    chai.request(app)
+      .get('/pi')
+      .end((err, res) => {
+        res.should.have.property('status').eql(400);
+        res.body.should.have.property('message').eql('Sorry this router does not exist !');
+        done();
+      });
+  });
 });
