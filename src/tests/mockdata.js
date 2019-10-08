b21eb6a648a7ca275f70f820d8e845dc65135265
nignanthomas <nignanthomas@gmail.com>
2019-10-08 11:11:35 +0200

    feature(signup/register): Signup/Register feature
    - create integration tests to test the signup endpoint
    
    - edit the models to add password and isVerified fields
    
    - create validators for signup request body
    
    - create helpers/utils for hashPassord, generate JWT token, verifyEmail and verifyUsername
    
    - create signupController and export to routes
    
    - create midllewares for checkSignup for the req.body, verifyEmail, verifyUsername, confirmPassword
    
    - create routes for 'api/v1/register/' with the aforementioned middlewares and signupController
    
    - change node version in Dockerfile from node:11-alpine to node:11 due to bcrypt crash
    
    [Finishes #168781675]

---
+++ src/tests/mockdata.js
@@ -0,0 +1,38 @@
+const testdata = {
+  validSignup: {
+    username: 'caretdevs',
+    email: 'teamcaret@gmail.com',
+    password: 'Pa$5W0rd',
+    confirmPassword: 'Pa$5W0rd',
+  },
+
+  missingEmail: {
+    username: 'caretdevs1',
+    password: 'Pa$5W0rd',
+    confirmPassword: 'Pa$5W0rd',
+  },
+
+  emailExisting: {
+    username: 'caretdevs2',
+    email: 'teamcaret@gmail.com',
+    password: 'Pa$5W0rd',
+    confirmPassword: 'Pa$5W0rd',
+  },
+
+  usernameExisting: {
+    username: 'caretdevs',
+    email: 'teamcaret3@gmail.com',
+    password: 'Pa$5W0rd',
+    confirmPassword: 'Pa$5W0rd',
+  },
+
+  passwordNotMatch: {
+    username: 'caretdevs4',
+    email: 'teamcaret4@gmail.com',
+    password: 'Pa$5W0rd',
+    confirmPassword: 'Pa$5W0rDD',
+  },
+
+};
+
+export default testdata;
