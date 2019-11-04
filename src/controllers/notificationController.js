0ae670a76caef2eeddf229af7a6e272f399e3d22
Macheza Dzabala <dzabalamacheza@gmail.com>
2019-11-04 16:31:15 +0200

    FT(Users Mark All Notifications): All users Mark Notifications
    - Users Can mark all notifications as read
    
    [finishes #168781698]

--- src/controllers/notificationController.js
+++ src/controllers/notificationController.js
@@ -8,12 +8,11 @@ const { NOTIF_NOT_FOUND, NOTIF_FOUND } = strings.notifications;
 export default class notificationController {
 
   static async allNotifications(req, res) {
-    const notifications = await allNotif(req.user.payload.id);
-
+    const query = { where: { userNotified: req.user.payload.id } };
+    const notifications = await allNotif(query);
     if (!notifications.length) {
       return responseUtil(res, 404, NOTIF_NOT_FOUND);
     }
-
     return responseUtil(res, 200, NOTIF_FOUND, notifications);
   }
 
@@ -28,14 +27,44 @@ export default class notificationController {
   }
 
   static async switchRead(req, res) {
-    const updatedNotif = await updateNotif(parseInt(req.params.id, 10), req.user.payload.id);
+    const { id } = req.params;
+    const userNotified = req.user.payload.id;
+    const query = {
+      where: {
+        id,
+        userNotified,
+      }
+    };
 
-    if (!updatedNotif) {
-      return responseUtil(res, 404, NOTIF_NOT_FOUND);
+    try {
+      const updatedNotif = await updateNotif(query);
+      const status = (updatedNotif.isRead) ? '' : 'un';
+      const message = `This notification status has been marked as ${status}read`;
+      return responseUtil(res, 200, message);
+    } catch (error) {
+      return responseUtil(res, 404, error.message);
     }
+  }
 
-    const status = (updatedNotif.isRead) ? '' : 'un';
-    const message = `This notification status has been marked as '${status}read'`;
-    return responseUtil(res, 200, message);
+  static async markAllRead(req, res) {
+    const { id } = req.user.payload;
+    const query = { where: { userNotified: id, isRead: false } };
+    const notifications = await notifServices.allNotif(query);
+    if (notifications.length === 0) {
+      return responseUtil(res, 409, 'You have no unread notifications');
+    }
+    Promise.all(notifications.map(async notification => {
+      await notification.update({
+        isRead: true
+      }).then(notification => {
+        try {
+          return notification.save();
+        } catch (error) {
+          return responseUtil(res, 500, 'Unable to complete', error.message);
+        }
+      });
+    }))
+      .then(() => responseUtil(res, 200, 'marked all notifications as read'))
+      .catch(error => responseUtil(res, 500, 'Unable to complete', error.message));
   }
 }
